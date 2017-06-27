5fff876ab23ad892b9d4f808048d85a632397e78
Egor Antonov <me@eantonov.name>
2017-06-27 16:27:32 +0300

    server config

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ import cherrypy
 from cherrypy.lib.static import serve_file
 import os
 import ujson as json
-from collab import CollabPredict
+from collab import CollabPredict, Stats
 
 class WordPredict(object):
     def __init__(self):
+        cherrypy.log('Initializing methods...')
         self.methods = {
-            'collab': CollabPredict.load('collab.model'),
+            #'collab': CollabPredict.load('collab.model'),
         }
 
     @cherrypy.expose
@@ -18,19 +19,30 @@ class WordPredict(object):
         return serve_file('static/index.html')
 
     @cherrypy.expose
-    @cherrypy.tools.json_out
     def get_predicted_words(self, seeds):
         jdata = json.loads(seeds)
+        res = []
+        for method_name, method in self.methods.items():
+            res.append({
+                method_name: [method.predict(jdata)]
+            })
 
 
 
 def main(args):
-    cherrypy.quickstart(WordPredict(), '/skyeng', config={
+    cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
         'server.socket_port': args.port,
+        'log.access_file': './access.log',
+        'log.error_file': './error.log',
         'tools.staticdir.on': True,
         'tools.staticdir.dir': '/home/site/skyeng/static',
         'tools.staticdir.index': 'index.html'
     })
+    cherrypy.quickstart(
+        WordPredict(),
+        '/skyeng'
+    )
     
     
 if __name__ == '__main__':
