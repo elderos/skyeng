3e1faf34c9e5650031a49972187d031996d5f10c
Egor Antonov <elderos@yandex-team.ru>
2017-06-30 13:58:07 +0300

    run script

---
+++ run_server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export CUDA_INC_DIR=/usr/local/cuda
+export CUDA_HOME=/usr/local/cuda
+export LD_LIBRARY_PATH=:/usr/local/cuda/lib64:/usr/lib/cuda:/usr/lib/cuda/lib64:/usr/local/lib/x86_64-linux-gnu:/usr/local/cuda/targets/x86_64-linux/lib:/usr/local/cuda/lib64:/home/elderos/soft/cuda:/home/elderos/soft/cuda/lib64
+export PATH=/home/elderos/bin:/home/elderos/.local/bin:/usr/local/cuda/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/home/elderos/bin:/usr/local/cuda/targets/x86_64-linux/include:/usr/lib/gcc/x86_64-linux-gnu/4.9:/home/elderos/soft/cuda/include
+
+python server.py -p 50000
